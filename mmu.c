5f6bf21ec1c6528c1169383f8fa92fee8129f12c
gabsereniski <gabsereniski@gmail.com>
2023-07-02 23:28:29 -0300

    ascii art desnecessaria pq tava afim

--- mmu.c
+++ mmu.c
@@ -30,19 +30,47 @@ void reset_table_entry(int pti)
 
 void print_table()
 {
-    printf("page | v | r | m | frame\n");
+    printf(" --------------------------\n");
+    printf("| PAGE TABLE               |\n");
+    printf("|--------------------------|\n");
+    printf("| page | v | r | m | frame |\n");
     for(int i = 0; i < total_virtual_pages; i++)
     {
         table_entry te = page_table[i];
-        printf("%4d | %d | %d | %d | %d\n", i, te.v, te.r, te.m, te.frame);
+        printf("| %4d | ", i);
+        printf("%s | ", te.v ? "1" : " ");
+        printf("%s | ", te.r ? "1" : " ");
+        printf("%s | ", te.m ? "1" : " ");
+        te.v ? printf("%5d |\n", te.frame) : printf("      |\n");
     }
+    printf(" --------------------------\n");
 }
 
 void print_ram()
 {
-    printf("frame | data\n");
+    printf(" --------------\n");
+    printf("| RAM CONTENT  |\n");
+    printf("|--------------|\n");
+    printf("| frame | data |\n");
     for(int i = 0; i < ram_size; i++)
-        printf("%5d | %d\n", i/page_size, ram[i]);
+    {
+        printf("| %5d | ", i/page_size);
+        ram[i] == -1 ? printf("     |\n") : printf("%4d |\n", ram[i]);
+    }
+        
+    printf(" --------------\n");
+}
+
+void ram_access(int pti, int logical_address, op_code op)
+{
+    int offset = OFFSET(logical_address);
+    int ra = RAM_ADDRESS(page_table[pti].frame, offset);
+    if(op == WRITE)
+    {
+        printf("write to %x\n", ra);
+        page_table[pti].m = true;
+    }
+    printf("virtual address 0x%x -> physical address 0x%x\n\n", logical_address, ra);
 }
 
 int find_free_slot()
@@ -59,16 +87,9 @@ bool page_on_ram(int pti, int addr, op_code op)
 {
     if(page_table[pti].v == false) return false;
 
-    printf("on ram\n");
-    int offset = OFFSET(addr);
-    int ra = RAM_ADDRESS(page_table[pti].frame, offset);
-    printf("offset = %d, virtual 0x%x >> physical 0x%x\n", offset, addr, ra);
-    page_table[pti].r = true;
-    if(op == WRITE)
-    {
-        printf("write to %x\n", ra);
-        page_table[pti].m = true;
-    }
+    printf("page %d on ram\n", pti);
+    
+    ram_access(pti, addr, op);
 
     return true;
 }
@@ -85,25 +106,17 @@ void update_ram(int logical_address, int physical_address)
 
 void update_table(int pti, int logical_address, op_code op)
 {
+    printf("added page %d to ram\n", pti);
     page_table[pti].v = true; // indico que agora a posicao eh valida
     page_table[pti].r = true; // indico que houve acesso
 
-    int offset = OFFSET(logical_address);
-    int ra = RAM_ADDRESS(page_table[pti].frame, offset);
-    printf("offset = %d. virtual 0x%x >> physical 0x%x\n", offset, logical_address, ra);
-
-    if(op == WRITE)
-    {
-        printf("write to %x\n", ra);
-        page_table[pti].m = true;
-    }
+    ram_access(pti, logical_address, op);
 }
 
 int fifo_policy(int pti)
 {
     int victim = queue_front(&fifo);
     queue_pop(&fifo);
-    // queue_push(&fifo, pti);
     return victim;    
 }
 
@@ -118,8 +131,8 @@ void simulation()
 
         print_table();
         print_ram();
-        printf("total page fault: %d\n", page_fault_count);
-        printf("------ pressione tecla para continuar\n");
+        printf("total page faults: %d\n", page_fault_count);
+        printf("\n--------------------- press key to continue\n");
         getchar();
         printf("op: %s | ", operation == READ ? "r" : "w");
         printf("address: %d\n", addr);
@@ -131,16 +144,15 @@ void simulation()
         // atualizo a fila do fifo
         if(algorithm == FIFO) queue_push(&fifo, pti);
 
-        printf("not on ram\n");
+        printf("not on ram - page fault\n");
         page_fault_count++;
 
         // verifico se ha espaco livre
         int slot = find_free_slot();
-        printf("free slot = %d\n", slot);
         // se encontro, copio as informacoes do disco para a ram
         if(slot != -1)
         {
-            printf("espaco livre\n");
+            printf("free space on ram\n");
             // indico que apartir de agora o frame se encontra no slot anteriormente vazio
             page_table[pti].frame = slot / page_size;
             update_table(pti, addr, operation);            
@@ -149,7 +161,6 @@ void simulation()
             continue;
         }
 
-        printf("substituicao\n");
         // se nao ha free slot, preciso substituir um deles
         // escolhe a pagina vitima de acordo com a politica de substituicao
         int victim;
@@ -159,15 +170,17 @@ void simulation()
             case LRU: break;
             case SECOND_CHANCE: break;
         }
-
         
         page_table[pti].frame = page_table[victim].frame;
         slot = page_table[victim].frame * page_size;
+        printf("page %d was chosen to be removed\n", victim);
+        if(page_table[victim].m)
+            printf("page %d was modified, write to disk\n", victim);
         reset_table_entry(victim);
         update_table(pti, addr, operation);
         update_ram(addr, slot); 
     }
     print_table();
     print_ram();
-    printf("total page fault: %d\n", page_fault_count);
+    printf("total page faults: %d\n", page_fault_count);
 }
